84294436474a35e18fb196d242b431bea1a7dec0
Homersmyid <48653956+Homersmyid@users.noreply.github.com>
2019-05-25 20:51:44 -0400

    Add files via upload

---
+++ Tests/24Bus/ruizC.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+#Constants
+SOLVER = "cplex"			#Solver to use
+EPSILON = 1e-6				#Distance between up and lower bound to halt
+DATA = "data.dat"			#Problem data file 
+UNCTOL = 1e-9			#Tolerance to use for uncertainity denominators
+MIPGAP = 1e-6				#Solver Gap in mixed integer problem
+
+
