5073a8870a31ce2896a56154478c95187cc260d2
Homersmyid <48653956+Homersmyid@users.noreply.github.com>
2019-05-27 00:08:31 -0400

    Update ruizC.py

--- ruizC.py
+++ ruizC.py
@@ -1,10 +1,18 @@
+###############################################################
+#Constants
+#Constants for the Constraint-and-Column method
+#Input  -	None
+#Output -   None
+#See Readme, section on ruizC for details
+###############################################################
+
 # -*- coding: utf-8 -*-
 
 #Constants
 SOLVER = "cplex"			#Solver to use
 EPSILON = 1e-7				#Distance between up and lower bound to halt
 DATA = "data.dat"			#Problem data file 
-UNCTOL = -1e-12			#Tolerance to use for uncertainity denominators
+UNCTOL = -1e-12			    #Tolerance to use for uncertainity denominators
 MIPGAP = 1e-12				#Solver Gap in mixed integer problem
 
 
